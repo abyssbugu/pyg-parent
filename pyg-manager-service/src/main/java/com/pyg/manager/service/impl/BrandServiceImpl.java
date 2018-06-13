25b1dbff2b84ccd32ebb5b9a1b97696e1027c1b6
Abyss <547161555@qq.com>
2018-06-13 21:22:24 +0800

    [BugFix]dubbo远程调用

--- pyg-manager-service/src/main/java/com/pyg/manager/service/impl/BrandServiceImpl.java
+++ pyg-manager-service/src/main/java/com/pyg/manager/service/impl/BrandServiceImpl.java
@@ -1,10 +1,10 @@
 package com.pyg.manager.service.impl;
 
+import com.alibaba.dubbo.config.annotation.Service;
 import com.pyg.manager.service.BrandService;
 import com.pyg.mapper.BrandMapper;
 import com.pyg.pojo.TbBrand;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
---
+++ pyg-manager-service/src/test/java/com/pyg/manager/service/impl/BrandServiceImplTest.java
@@ -0,0 +1,36 @@
+package com.pyg.manager.service.impl;
+
+import com.pyg.manager.service.BrandService;
+import com.pyg.mapper.BrandMapper;
+import com.pyg.pojo.TbBrand;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Abyss on 2018/6/13.
+ * description:
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:spring/applicationContext-dao.xml")
+public class BrandServiceImplTest {
+
+
+    @Autowired
+    private BrandMapper brandMapper;
+
+    @Test
+    public void findAll() {
+        List<TbBrand> brands = brandMapper.findAll();
+        for (TbBrand brand : brands) {
+            System.out.println(brand.getName());
+        }
+    }
+}
